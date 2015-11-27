9730cd15a87b63d57df049a2f8f9a00288e2540b
James Charles Searson <j.c.searson@gmail.com>
2015-11-27 02:36:25 -0500

    New Syntax Highlighter

--- js/global.js
+++ js/global.js
@@ -3,13 +3,14 @@ var $searchGreen = $( '.search-container' );
 $currentScreen = $(window);
 
 $currentScreen.scroll(function () {
-	var scroll = $currentScreen.scrollTop();
+  'use strict';
+  var scroll = $currentScreen.scrollTop();
 
-	if (scroll >= 100) {
-		$logoBlock.addClass( "light-up" ),
-		$searchGreen.addClass( " make-blue" );
-	} else {
-		$logoBlock.removeClass( "light-up" ),
-		$searchGreen.removeClass( " make-blue" );
-	}
+  if (scroll >= 100) {
+    $logoBlock.addClass( "light-up" ),
+    $searchGreen.addClass( " make-blue" );
+  } else {
+    $logoBlock.removeClass( "light-up" ),
+    $searchGreen.removeClass( " make-blue" );
+  }
 });
